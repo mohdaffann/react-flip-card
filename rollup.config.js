585cb5b21841b8247e4b2b3408018226c0a92863
muhammadaffan <muhammadaffanpvt@gmail.com>
2025-08-19 23:24:31 +0530

    ts , rollup and json configs

---
+++ rollup.config.js
@@ -0,0 +1,27 @@
+import typescript from "rollup-plugin-typescript2";
+import postcss from "rollup-plugin-postcss";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+
+export default {
+    input: "src/index.ts",
+    output: [
+        {
+            file: "dist/index.cjs.js",
+            format: "cjs",
+            sourcemap: true,
+        },
+        {
+            file: "dist/index.esm.js",
+            format: "esm",
+            sourcemap: true,
+        },
+    ],
+    plugins: [
+        resolve(),
+        commonjs(),
+        typescript({ tsconfig: "./tsconfig.json" }),
+        postcss({ extract: "index.css" }),
+    ],
+    external: ["react", "react-dom"],
+};
