4564516cb634e7687a52f2b75bee1f99bda5281d
muhammadaffan <muhammadaffanpvt@gmail.com>
2025-08-19 23:25:58 +0530

    testing front and back cards

---
+++ test-app/src/Cards/BackDiv.jsx
@@ -0,0 +1,8 @@
+function BackDiv() {
+    return (
+        <div className="w-full h-full flex items-center justify-center bg-black text-white">
+            Black
+        </div>
+    );
+}
+export default BackDiv;
---
+++ test-app/src/Cards/FrontDiv.jsx
@@ -0,0 +1,15 @@
+function FrontDiv() {
+    return (
+        <div className="relative w-full">
+            <img
+                src="/Eiffel.jpg"
+                className="w-full h-[400px] object-cover"
+                alt="Eiffel"
+            />
+            <h3 className="absolute bottom-2 left-2 text-white text-xl font-bold drop-shadow">
+                CHAFFEUR
+            </h3>
+        </div>
+    );
+}
+export default FrontDiv;
